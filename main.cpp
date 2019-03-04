1db216f8a204cc69738118fa4f7d77cf6dc6c58e
GeoDTN <tadewos85@gmail.com>
2019-03-05 00:10:29 +0100

    Input values for hobbies changed

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ int main()
     sn.addUser("James",0,170,"male",{"","",""},2,{"Andrew","John"} );
     sn.addUser("Somano",20,170,"male",{"","",""},3,{"John","Tadewos"} );
     sn.addUser("Somano",20,170,"male",{"Foot ball","",""},4,{"John","Tadewos"} );
+    sn.addUser("Somano",20,170,"male",{"Foot ball","",""},5,{"John","Benjamin","George","Akmos"} );
+    sn.addUser("Somano",20,170,"male",{"Volley Ball","Joker","Table tennis","Soccer"},6,{"John","Tadewos"} );
     sn.deleteUser(3);
     return 0;
 
